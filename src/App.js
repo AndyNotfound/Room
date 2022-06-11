d03418fec09bca8d7ef1df7a32fac758e880d8be
AndyNotfound <Andy.Notfound@gmail.com>
2022-06-11 17:19:54 +0700

    hero setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Hero from "./components/hero/hero";
 import NavigationBar from "./components/navigationBar/navigationBar";
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <Hero />
     </div>
   );
 }
