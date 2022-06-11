9c045051d985be01437c70ee0b8a84209133f8c9
AndyNotfound <Andy.Notfound@gmail.com>
2022-06-12 04:25:55 +0700

    css setup

--- src/components/navigationBar/navigationBar.jsx
+++ src/components/navigationBar/navigationBar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { GiHamburgerMenu} from 'react-icons/gi'
 import { AiOutlineClose } from 'react-icons/ai'
 import logo from '../../images/logo.svg'
-import '../scss/style.scss'
+import '../scss/style.css'
 
 function NavigationBar() {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -12,6 +12,9 @@ function NavigationBar() {
   };
   return (
     <nav id='mainNavigation' className='mainNavigation'>
+      <button onClick={toggleHandler} className="toggleMenu">
+        {toggleMenu? <AiOutlineClose /> : <GiHamburgerMenu />}
+      </button>
       <img src={logo} alt='Room company logo' className='mainNavigationLogo' />
       <ul className={toggleMenu? 'navLinks navLinksActive' : 'navLinks'}>
         <li><a href="">home</a></li>
@@ -19,9 +22,6 @@ function NavigationBar() {
         <li><a href="">about</a></li>
         <li><a href="">contact</a></li>
       </ul>
-      <button onClick={toggleHandler} className="toggleMenu">
-        {toggleMenu? <AiOutlineClose /> : <GiHamburgerMenu />}
-      </button>
     </nav>
   )
 }
